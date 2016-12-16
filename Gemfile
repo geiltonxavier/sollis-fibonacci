4c480faa53702ef9f72f4c2ece84a26af5cf55f4
Geilton Xavier <geilton@live.com>
2016-12-15 23:03:24 -0200

    add rack-cors to accept requests from other pages

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
+
+# Rack Middleware for handling Cross-Origin Resource Sharing (CORS), which makes cross-origin AJAX possible. 
+gem 'rack-cors'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,14 @@ Bundler.require(*Rails.groups)
 
 module SollisFibonacci
   class Application < Rails::Application
+
+  	  # Configuração CORS para aceitar chamadas de outras páginas
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
